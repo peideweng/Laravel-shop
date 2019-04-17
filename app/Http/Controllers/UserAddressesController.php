e800421b9625abe0237617a64fdf23d42e26172d
vagrant <vagrant@homestead>
2019-04-17 15:16:34 +0000

    删除和修改收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -34,4 +34,33 @@ class UserAddressesController extends Controller
 
         return redirect()->route('user_addresses.index');
     }
+
+    public function edit(UserAddress $userAddress)
+    {
+        $this->authorize('own', $userAddress);
+        return view('user_addresses.create_and_edit', ['address' => $userAddress]);
+    }
+
+    public function update(UserAddress $userAddress, UserAddressRequest $request)
+    {
+        $this->authorize('own', $userAddress);
+        $userAddress->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    public function destory(UserAddress $userAddress)
+    {
+        $this->authorize('own', $userAddress);
+        $userAddress->delete();
+        return [];
+    }
 }
