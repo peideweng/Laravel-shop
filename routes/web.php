2290c618e4bfe780c1fb7ff1298d2685bba573a0
vagrant <vagrant@homestead>
2019-04-17 12:27:16 +0000

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,11 @@ Auth::routes(['verify' => true]);
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function () {
+    // 用户收货地址
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    // 用户添加收货地址
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    // 用户添加收货地址
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+
 });
