9ecdc5181632e9a24297cb8cc27a5faefa6ba437
vagrant <vagrant@homestead>
2019-04-28 06:24:49 +0000

    订单支付

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        // 支付宝 服务端回调
+        'payment/alipay/nofify',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,15 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     // 订单页面
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+
+    // 订单 支付宝支付
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    // 订单 支付宝支付 前端回调
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
 });
+
+// 订单 支付宝支付 服务端回调
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 
 // 产品详情
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
